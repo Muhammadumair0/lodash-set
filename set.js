8eba967f163c5d0d6c067dd70ed94d8982d0b600
Muhammad Umair Khan <muhammadumairkhanyz@gmail.com>
2019-04-15 13:37:44 +0500

    Update set.js

--- set.js
+++ set.js
@@ -1,20 +1,37 @@
+let obj = { a: [{ b: { c: 3 } }] };
+
+// let path = ['a', '0', 'b', 'c'];
+// let path = 'a[0].b.c';
+
 function set(object, path, value) {
   if (typeof path === "string") {
+    try {
       eval(`object.${path} = ${value}`);
+    } catch (e) {}
   }
 
-  if(path.constructor === Array) {
-     let stringPath = path.reduce((acc, item) => {
-         if(Number(item) >= 0) {
-            return acc + `[${item}]`;
-        } else {
-            return acc + `.${item}`
-         }
-      }, '');
+  if (path.constructor === Array) {
+    let stringPath = path.reduce((acc, item) => {
+      return (
+        acc + `[${Number(item) >= 0 || Number(item) < 0 ? item : `'${item}'`}]`
+      );
+    }, "");
+    try {
       eval(`object${stringPath} = ${value}`);
+    } catch (e) {
+      generateDynamicProperty(obj, path);
+      eval(`object${stringPath} = ${value}`);
+    }
+  }
+
+  function generateDynamicProperty(obj, path) {
+    let value = 0;
+    lastKeyIndex = path.length - 1;
+    for (var i = 0; i < lastKeyIndex; ++i) {
+      key = path[i];
+      if (!(key in obj)) obj[key] = {};
+      obj = obj[key];
+    }
+    obj[path[lastKeyIndex]] = value;
   }
 }
-
-// let object = { 'a': [{ 'b': { 'c': 3 } }] };
-// stringPath = 'a[0].b.c';
-// arrayPath = ['a', '0', 'b', 'c'];
